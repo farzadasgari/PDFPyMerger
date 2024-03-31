7b4871ef6974665a56713842b791721ac50cb245
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-04-01 03:13:50 +0330

    Update encrypt.py docStrings

--- encrypt.py
+++ encrypt.py
@@ -2,10 +2,13 @@ from PyQt5.QtWidgets import QLineEdit
 
 
 def hide_or_show_password(passinput) -> None:
-    '''
-    This function is used to change the password mode to Hide or Show. 
-    By pressing the eye button, this mode change is displayed.
-    '''
+    """
+    Toggle between hiding and showing the password input.
+    
+    Parameters:
+        passinput (QLineEdit): The password input field.
+    """
+    
     if passinput.echoMode() == QLineEdit.EchoMode.Password:
         passinput.setEchoMode(QLineEdit.EchoMode.Normal)
     else:
