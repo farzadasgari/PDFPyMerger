bd4f00d45ce3e38d7eede0adb800dc3a42796458
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-04-01 03:35:08 +0330

    Update main_application.py docString

--- main_application.py
+++ main_application.py
@@ -1,21 +1,3 @@
-'''
-PDF Merger:
-
-This Python application designed to merge multiple PDF files into a single document with 
-the ability to protect the merged PDF file with a password.
-
-Required libraries:
-
-PyQt5 = PyQt is a Python binding of the cross-platform GUI toolkit Qt, implemented as a Python plug-in.
-
-Pillow = The Python Imaging Library adds image processing capabilities to your Python interpreter.
-
-PyPDF2 = PyPDF2 is a free and open source pure-python PDF library capable of splitting, merging, cropping, 
- and transforming the pages of PDF files.
-
-img2pdf = Convert images to PDF via direct JPEG inclusion.
-
-'''
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.uic import loadUiType
 from file_handling import get_files, delete_file
@@ -24,15 +6,26 @@ from encrypt import hide_or_show_password
 from exec import merger
 from info_dialog import info_btn_clicked
 
-# Load Graphical User Interface
+# Load Graphical User Interface from GUI file
 gui, _ = loadUiType("gui.ui")
 
 
 class PyPDFMerger(QMainWindow, gui):
-    '''
-    The application User Interface (UI) and widgets functionalities
-    '''
+    """
+    Main application window for PDFPyMerger.
+
+    Attributes:
+        Inherits from QMainWindow and gui (loaded UI type).
+
+    Methods:
+        __init__: Initialize the PyPDFMerger window and connect signals to slots.
+        buttons_clicked: Connect button click events to corresponding functions.
+        actions_triggered: Connect menu actions to corresponding functions.
+    """
     def __init__(self):
+        """
+        Initialize the PyPDFMerger window and connect signals to slots.
+        """
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.hidePasswordButton.hide()
@@ -41,6 +34,9 @@ class PyPDFMerger(QMainWindow, gui):
         self.buttons_clicked()
 
     def buttons_clicked(self):
+        """
+        Connect button click events to corresponding functions.
+        """
         self.addPDFButton.clicked.connect(lambda: get_files(self.PDFList))
         self.deletePDFButton.clicked.connect(lambda: delete_file(self.PDFList))
         self.hidePasswordButton.clicked.connect(lambda: hide_or_show_password(self.passwordInput))
@@ -50,4 +46,7 @@ class PyPDFMerger(QMainWindow, gui):
         self.executeButton.clicked.connect(lambda: merger(self))
 
     def actions_triggered(self):
+        """
+        Connect menu actions to corresponding functions.
+        """
         self.actionInfo.triggered.connect(info_btn_clicked)
