269b8eefbdba7c117f8bc6942817061a72b25a3b
ZarRza <160187613+ZarRza@users.noreply.github.com>
2024-04-06 00:51:10 +0430

    practice for pull request.

--- encrypt.py
+++ encrypt.py
@@ -1,9 +1,13 @@
 from PyQt5.QtWidgets import QLineEdit
 
 
+
+
 def hide_or_show_password(passinput) -> None:
     """
     Toggle between hiding and showing the password input.
+     
+    
     
     Parameters:
         passinput (QLineEdit): The password input field.
